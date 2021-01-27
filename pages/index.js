e18c88d6e01329fa8e9381b2fd5ab52f4586f221
AndersKjoelberg97 <anders_bk123@live.no>
2021-01-27 19:09:53 +0100

    testing api status

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+//import styles from '../styles/Home.module.css'
 import InputField from '../components/InputField'
 
 export default function Home() {

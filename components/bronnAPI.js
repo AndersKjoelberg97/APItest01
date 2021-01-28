2efafa6880956bfbe6a6dc1ba31d9cde7d861f9c
AndersKjoelberg97 <anders_bk123@live.no>
2021-01-28 13:06:10 +0100

    lagt til fungerende søkefelt

--- components/bronnAPI.js
+++ components/bronnAPI.js
@@ -5,20 +5,32 @@ import axios from 'axios'
 const BronnAPI = () => {
  
     const [bronnListe, setBronnList] = useState([])
+    const [innboks, setInnboks] = useState('')
 
     const getBronndata = (e) => {
         e.preventDefault()
-        
-        axios.get('https://data.brreg.no/enhetsregisteret/api/enheter/')
+
+        if (innboks.length == 0){
+
+            axios.get('https://data.brreg.no/enhetsregisteret/api/enheter/')
             .then(res => setBronnList(res.data._embedded.enheter))
             .catch(err => console.log(err))
+        }
+        else
+        {
+            axios.get(`https://data.brreg.no/enhetsregisteret/api/enheter/?navn=${innboks}`)
+            .then(res => setBronnList(res.data._embedded.enheter))
+            .catch(err => console.log(err))
+        }
+        
+        
     }
 
     return (
         <div className='knapper'>
             
             
-            <input className="inputboks"></input>
+            <input onChange={event => setInnboks(event.target.value)} />
             
             <button onClick={getBronndata}>søk</button>
             {
@@ -26,13 +38,13 @@ const BronnAPI = () => {
                     return <p key={idx}> {bronn.organisasjonsnummer + "- " + 
                         bronn.navn + "- Adresse: " +
                         bronn.forretningsadresse.postnummer + " " +
-                        bronn.forretningsadresse.kommune + "- Er de konkurs?: " +
+                        bronn.forretningsadresse.kommune + "- Er de konkurs? " +
                         bronn.konkurs + "- Antall ansatte: " +
-                        bronn.antallAnsatte + "- Næringskode: " + 
-                        bronn.naeringskode1.kode + "- Form: " +
-
-                        bronn.organisasjonsform + "- " +
-                        bronn.sisteInnsendteAarsregnskap
+                        bronn.antallAnsatte + "- siste årsregnskap: " + 
+                        bronn.sisteInnsendteAarsregnskap + " " +
+                        
+                        //bronn.naeringskode1.kode + "- Form: " +
+                        bronn.organisasjonsform + "- " 
 
                     }</p>
                 })
